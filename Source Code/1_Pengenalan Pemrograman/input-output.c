7169a98d0bb0a815ed7a754dcea5fcc22fcfc4ab
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2022-10-03 14:25:56 +0000

    first day commit

---
+++ Source Code/1_Pengenalan Pemrograman/input-output.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Masukkan nilai variabel a: ");          
+    scanf("%d", &a);                                // input
+    printf("variabel a memiliki nilai = %d", a);    // output
+    return 0;
+}
---
+++ Source Code/1_Pengenalan Pemrograman/struktur-bahasa-c.c
@@ -0,0 +1,7 @@
+#include <stdio.h>              // header file
+                                // global declaration    
+int main()                      // fungsi utama (main)
+{                               // pembuka fungsi
+    printf("Hello World\n");     // statement
+    return 0;                    // statement
+}                               // penutup fungsi
